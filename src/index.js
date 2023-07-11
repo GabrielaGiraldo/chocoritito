364ab9bef90c106a3343317bc1cdeac1b5be4e7b
unknown <giraldogabriela861@gmail.com>
2023-07-11 18:25:20 -0500

    actividad

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDom from 'react-dom/client'
+import "./index.css"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDom.createRoot(document.getElementById('root'))
+function Saludo(){
+    return <div class = "general">
+        <div class = "hola">
+            <h2>Hola</h2>
+        </div>
+        <div class = "nombre">
+            <h3>My name is Clementina</h3>
+        </div>
+        <div class = "info">
+            <p>I'm a little boy that works in little projects. Thanks for support. I'm Working On It.</p>
+        </div>
+    </div>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+}
+
+root.render(<div>
+    {Saludo()}
+</div>)
